abc9e26155646934f1505e74422f47948fb60437
Wallyngson Guedes <33876147+wallyngson@users.noreply.github.com>
2021-02-24 23:52:11 -0300

    finish-challengebox

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,39 @@
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox() {
+  
+  const hasActiveChallenge = true
+  
   return (
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengeBoxNotActive}>
-        <strong>Começe um novo ciclo para receber desafios a serem completados</strong>
-        <p>
-          <img src="icons/level-up.svg" alt="level up"/>
-          Avance de levels completando desafios.
-        </p>
-      </div>
+      { hasActiveChallenge ? (
+        <div className={styles.challengerActive}>
+          <header>Ganhe 410xp</header>
+
+          <main>
+            <img src="icons/body.svg" alt="body"/>
+            <strong>Novo desafio</strong>
+            <p>Levante e faça 10 poli-chinelos.</p>
+          </main>
+
+          <footer>
+            <button 
+              type="button"
+              className={styles.challengeSuceededButton}>Completei</button>
+            <button 
+              type="button"
+              className={styles.challengeFailButton}>Falhei</button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeBoxNotActive}>
+          <strong>Começe um novo ciclo para receber desafios a serem completados</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="level up"/>
+            Avance de levels completando desafios.
+          </p>
+        </div>
+      ) }
 
     </div>
   ) 
