fab2e9b698db184db6c8b6657ef7b51b4973d152
Wallyngson Guedes <33876147+wallyngson@users.noreply.github.com>
2021-02-27 21:38:36 -0300

    finish-cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,9 @@ import { ChallengesProvider } from '../contexts/ChallengesContext';
 function MyApp({ Component, pageProps }) {
 
   return (
-    <ChallengesProvider>
-        <Component {...pageProps} />
-    </ChallengesProvider>
+    
+    <Component {...pageProps} />
+
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GetServerSideProps } from 'next';
 
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExperienceBar } from "../components/ExperienceBar";
@@ -7,33 +8,63 @@ import { Countdown } from "../components/Countdown";
 import { ChallengeBox } from "../components/ChallengeBox";
 
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
 import styles from "../styles/pages/Home.module.css";
 import Head from 'next/head';
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengerCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
+    <ChallengesProvider 
+      level={props.level}  
+      currentExperience={props.currentExperience}
+      challengerCompleted={props.challengerCompleted}
+      >
+        <div className={styles.container}>
+          <Head>
+            <title>Início | move.it</title>
+          </Head>
+          
+          <ExperienceBar />
+          
+          <CountdownProvider>
+            <section>
+              <div>
+                <Profile />
+                <CompletedChallenges />
+                <Countdown />
+              </div>
+              <div>
+                <ChallengeBox />
+              </div>
+            </section>
+          </CountdownProvider>
 
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      
-      <ExperienceBar />
-      
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-
-    </div>   
+        </div>
+    </ChallengesProvider>
   );
+};
+
+
+/*  
+    Esse metódo precisa ser escrito desta forma e ela controla
+    o Next.js irá mandar para o Front-end em React.js
+*/
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const { level, currentExperience, challengerCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengerCompleted: Number(challengerCompleted),
+    }
+  }
 };
